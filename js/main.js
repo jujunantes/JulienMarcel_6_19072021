8c8e46917da8ff60a0f6b3da243a54742972d5b1
Julien MARCEL <jul_marcel@hotmail.com>
2021-07-28 10:49:04 +0200

    cartes sans reflow

--- js/main.js
+++ js/main.js
@@ -10,11 +10,13 @@ fetch('js/FishEyeData.json')
   .then((reponse) => reponse.json())
   .then(function (donnees){
 
-    for(var chaquePhotographe of donnees.photographers) {
+    let monHTML = '';
+
+    for(let chaquePhotographe of donnees.photographers) {
         //console.log(chaquePhotographe.name);
-        var photographe = creePhotographe(chaquePhotographe);
+        let photographe = creePhotographe(chaquePhotographe);
         // On peut maintenant ajouter la carte de chaque photographe
-        document.getElementById('main').innerHTML += `
+        monHTML += `
         <a href="profil.html?id=${photographe.id}" aria-label="Affichez le profil de ${photographe.name}">
             <figure class="cartePhotographe" tabindex="">
                 <img class="photoProfil" tabindex="" src="img/Sample_Photos/Photographers_ID_Photos/${photographe.portrait}" alt="" />
@@ -23,14 +25,15 @@ fetch('js/FishEyeData.json')
                     <h3 tabindex="" aria-label="Le photographe viens de ${photographe.city}">${photographe.city}, ${photographe.country}</h3>
                     <blockquote tabindex="" aria-label="La devise du photographe est :${photographe.tagline}">${photographe.tagline}</blockquote>
                     <p  tabindex="" aria-label="Le prix de ce photographe est ${photographe.price}€ par jour">${photographe.price}€ /jour</p>
-                    <div id="filtresPhotographes_${photographe.id}"></div>
+                    <div id="filtresPhotographes">`;
+        for(let tag of chaquePhotographe.tags) monHTML += `<span class="spanFiltres">#${tag}</span>`;
+        monHTML += `
+                    </div>
                 </figcaption>
             </figure>
-        </a>
-        `;
-        const filtres = document.getElementById("filtresPhotographes_"+photographe.id); //on nomme le filtre avec l'id du photographe sinon on bouclera sur les tags des autres photographes
-        for(var tag of chaquePhotographe.tags) filtres.innerHTML+= `<span class="spanFiltres">#${tag}</span>`;        
+        </a>`;     
     }
+    document.getElementById('main').innerHTML += monHTML;
 });
 
 /*
